217c0d57a8a09f9d88406e2cd74b56120974494b
leesh235 <leeshgh235@gmail.com>
2022-07-13 17:19:30 +0900

    frontend 세팅

--- src/server.js
+++ src/server.js
@@ -2,6 +2,14 @@ import express from "express";
 
 const app = express();
 
-console.log("hello");
+//pug사용
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views");
+//정적파일 등록
+app.use("/public", express.static(__dirname + "/public"));
+// "/" url만 사용할 수 있도록 설정
+app.get("/", (req, res) => res.render("home"));
 
-app.listen(3000);
+app.listen(3000, () => {
+    console.log("start server");
+});
