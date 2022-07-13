8392ca58f3fdc2b0c50a8a1fab54688d5acd9f98
leesh235 <leeshgh235@gmail.com>
2022-07-13 22:32:55 +0900

    websockets를 이용한 양방향 통신(간단한 chat기능)

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,45 @@
-alert("hi");
+const messageList = document.querySelector("ul");
+const nickForm = document.querySelector("#nick");
+const messageForm = document.querySelector("#message");
+//ws연결
+const socket = new WebSocket(`ws://${window.location.host}`);
+
+//json => string
+function makeMessage(type, payload) {
+    const msg = { type, payload };
+    return JSON.stringify(msg);
+}
+
+//ws연결 시
+socket.addEventListener("open", () => {
+    console.log("Connected to Server ✅");
+});
+
+//backend에서 send함수 실행 시
+socket.addEventListener("message", (message) => {
+    const li = document.createElement("li");
+    li.innerText = message.data;
+    messageList.append(li);
+});
+
+//server가 닫혔을 때
+socket.addEventListener("close", () => {
+    console.log("Disconnected from Server ❌");
+});
+
+function handleSubmit(event) {
+    event.preventDefault();
+    const input = messageForm.querySelector("input");
+    socket.send(makeMessage("new_message", input.value));
+    input.value = "";
+}
+
+function handleNickSubmit(event) {
+    event.preventDefault();
+    const input = nickForm.querySelector("input");
+    socket.send(makeMessage("nickname", input.value));
+    input.value = "";
+}
+
+messageForm.addEventListener("submit", handleSubmit);
+nickForm.addEventListener("submit", handleNickSubmit);
--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
+import http from "http";
+import WebSocket from "ws";
 import express from "express";
 
 const app = express();
@@ -8,8 +10,40 @@ app.set("views", __dirname + "/views");
 //정적파일 등록
 app.use("/public", express.static(__dirname + "/public"));
 // "/" url만 사용할 수 있도록 설정
-app.get("/", (req, res) => res.render("home"));
+app.get("/", (_, res) => res.render("home"));
+app.get("/*", (_, res) => res.redirect("/"));
 
-app.listen(3000, () => {
-    console.log("start server");
+const handleListen = () => console.log(`Listening on http://localhost:3000`);
+
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+const sockets = [];
+
+//connection event, 연결됐을 때 특정 행동
+wss.on("connection", (socket) => {
+    console.log("Connected to Browser ✅");
+
+    //각 브라우저마다 실행되므로 접속한 브라우저를 배열에 넣는다
+    sockets.push(socket);
+    //socket안에 data를 넣어줄 수 있다.
+    socket["nickname"] = "Anon";
+
+    //close event, 연결이 끊겼을 때 특정 행동
+    socket.on("close", () => console.log("Disconnected from the Browser ❌"));
+
+    //message event, 브라우저에서 socket send 함수에 대응하는 event
+    socket.on("message", (msg) => {
+        const message = JSON.parse(msg);
+        switch (message.type) {
+            case "new_message":
+                sockets.forEach((aSocket) =>
+                    aSocket.send(`${socket.nickname}: ${message.payload}`)
+                );
+            case "nickname":
+                socket["nickname"] = message.payload;
+        }
+    });
 });
+
+server.listen(3000, handleListen);
